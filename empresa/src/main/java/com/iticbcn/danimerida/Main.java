ed55aeb2b497d1d7b31e87a9551e68606f79c188
Dani <thos.merida.dani@gmail.com>
2025-01-31 18:55:07 +0100

    finalització 3.0 primera part de la pràctica

--- empresa/src/main/java/com/iticbcn/danimerida/Main.java
+++ empresa/src/main/java/com/iticbcn/danimerida/Main.java
@@ -1,19 +1,56 @@
 package com.iticbcn.danimerida;
 
-import org.hibernate.SessionFactory;
-
-
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import com.iticbcn.danimerida.model.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
         try (Session session = sessionFactory.openSession()) {
             Transaction tx = session.beginTransaction();
-
+            
+            // Crear un departamento
+            Departament departament = new Departament();
+            departament.setNom("Tus");
+            session.persist(departament);
+            
+            // Crear empleados
+            Empleat empleat1 = new Empleat();
+            empleat1.setNom("Anna Perez");
+            empleat1.setDepartament(departament);
+            session.persist(empleat1);
+            
+            Empleat empleat2 = new Empleat();
+            empleat2.setNom("Joan Martí");
+            empleat2.setDepartament(departament);
+            session.persist(empleat2);
+            
+            // Crear una tarea
+            Tasca tasca = new Tasca();
+            tasca.setDescripcio("Desenvolupar aplicació");
+            
+            // Asignar empleados a la tarea
+            Set<Empleat> empleats = new HashSet<>();
+            empleats.add(empleat1);
+            empleats.add(empleat2);
+            tasca.setEmpleats(empleats);
+            
+            session.persist(tasca);
+            
+            // Crear un histórico
+            Historic historic = new Historic();
+            historic.setTasca(tasca);
+            historic.setComentari("Inici de desenvolupament");
+            session.persist(historic);
+            
+            tx.commit();
+            System.out.println("Datos guardados correctamente!");
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
         } finally {
